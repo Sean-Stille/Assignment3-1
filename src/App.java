4dad90f3a4c47b2efce59afd50440498553f7fa4
MaxwellGrimm <thegrifter52@gmail.com>
2022-11-02 22:35:11 -0500

    diagonal

--- src/App.java
+++ src/App.java
@@ -1,5 +1,43 @@
+import java.util.ArrayList;
 public class App {
+
+    public static void fillOutDiagonal(int sideLength){
+        int diagonalLines = (sideLength + sideLength) - 1;
+        int midPoint = (diagonalLines / 2) + 1;
+        int[][] diagonal = new int[sideLength][sideLength];
+
+        int itemsInDiagonal = 0;
+        int p = 1;
+        for (int i = 1; i <= diagonalLines; i++) {
+            int rowIndex;
+            int columnIndex;
+
+            if (i <= midPoint) {
+                itemsInDiagonal++;
+                for (int j = 0; j < itemsInDiagonal; j++) {
+                    rowIndex = (i - j) - 1;
+                    columnIndex = j;
+                    diagonal[rowIndex][columnIndex] = p;
+                    p++;
+                }
+            } else {
+                itemsInDiagonal--;
+                for (int j = 0; j < itemsInDiagonal; j++) {
+                    rowIndex = (sideLength - 1) - j;
+                    columnIndex = (i - sideLength) + j;
+                    diagonal[rowIndex][columnIndex] = p;
+                    p++;
+                }
+            }
+        }
+        for(int i = 0; i < sideLength; i++){
+            for(int k = 0; k < sideLength; k++){ 
+                System.out.print(diagonal[k][i]);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        fillOutDiagonal(4);
     }
 }
